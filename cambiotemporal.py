c43ac57f8ece05d67d467ef2b3d37834010c1bdd
hadronnez <adrianordonezsanmartin@gmail.com>
2024-03-15 14:31:16 +0100

    sigo sigo main despues mergear login

--- cambiotemporal.py
+++ cambiotemporal.py
@@ -1,1 +1,6 @@
-He dejado un trabajo a medio hacer. No quiero hacer commit. Hago stash para guardarlo temporalmente. Esta es una edición que he hecho tras el commit. 
+
+
+He dejado un trabajo a medio hacer. No quiero hacer commit. Hago stash para guardarlo temporalmente. Esta es una edición que he hecho tras el commit.
+
+
+He seguido trabajando en este archivo.    Y un poco más 
